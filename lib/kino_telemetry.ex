a8d4828145af71cf29bf080180e1dcf159e9e21f
Michael Crumm <michael.crumm@cargosense.com>
2022-10-28 17:01:47 -0700

    Lift kino to main module

--- lib/kino_telemetry.ex
+++ lib/kino_telemetry.ex
@@ -1,18 +1,39 @@
 defmodule KinoTelemetry do
   @moduledoc """
-  Documentation for `KinoTelemetry`.
+  A kino wrapping a `Telemetry.Metrics` definition.
   """
+  alias VegaLite, as: Vl
+
+  defstruct [:metric, :pid, :vl]
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> KinoTelemetry.hello()
-      :world
-
+  Creates a new kino with the given `Telemetry.Metrics` definition.
   """
-  def hello do
-    :world
+  def new(metric) when is_struct(metric, Telemetry.Metrics.LastValue) do
+    chart_options = chart_options(metric)
+
+    chart =
+      Vl.new(chart_options)
+      |> Vl.mark(:line)
+      |> Vl.encode_field(:x, "x", type: :quantitative)
+      |> Vl.encode_field(:y, "y", type: :quantitative)
+      |> Kino.VegaLite.new()
+
+    {:ok, pid} = KinoTelemetry.Listener.listen([{chart, metric}])
+
+    %__MODULE__{metric: metric, vl: chart, pid: pid}
+  end
+
+  defp chart_options(%{reporter_options: options}) do
+    options
+    |> Keyword.take([:width, :height])
+    |> Keyword.put_new(:width, 400)
+    |> Keyword.put_new(:height, 400)
+  end
+
+  defimpl Kino.Render do
+    def to_livebook(chart) do
+      Kino.Render.to_livebook(chart.vl)
+    end
   end
 end
--- test/kino_telemetry_test.exs
+++ test/kino_telemetry_test.exs
@@ -1,8 +1,14 @@
 defmodule KinoTelemetryTest do
   use ExUnit.Case
-  doctest KinoTelemetry
 
-  test "greets the world" do
-    assert KinoTelemetry.hello() == :world
+  test "new/1 returns a KinoTelemetry for a LastValue metric" do
+    last_value = Telemetry.Metrics.last_value([:a, :b, :c])
+    kino = last_value |> KinoTelemetry.new()
+    assert %KinoTelemetry{metric: ^last_value} = kino
+  end
+
+  test "KinoTelemetry.Render.to_livebook/1" do
+    last_value = Telemetry.Metrics.last_value([:a, :b, :c])
+    assert {:js, _} = last_value |> KinoTelemetry.new() |> Kino.Render.to_livebook()
   end
 end
