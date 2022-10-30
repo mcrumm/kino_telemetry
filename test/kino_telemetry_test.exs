54acf61ce4aee43f0f69c6bca31dbfd628dd6a7b
Michael Crumm <michael.crumm@cargosense.com>
2022-10-30 16:20:48 -0700

    Add chart spec and reporting function tests

--- test/kino_telemetry_test.exs
+++ test/kino_telemetry_test.exs
@@ -4,7 +4,13 @@ defmodule KinoTelemetryTest do
 
   setup :configure_livebook_bridge
 
-  test "sends telemetry events after initial connection" do
+  test "last_value spec" do
+    kino = Telemetry.Metrics.last_value("a.b.c") |> KinoTelemetry.new()
+    data = connect(kino.vl)
+    assert %{spec: %{"mark" => %{"point" => true, "type" => "line"}}} = data
+  end
+
+  test "pushes measurements after initial connection" do
     last_value = Telemetry.Metrics.last_value("a.b.c")
     kino = last_value |> KinoTelemetry.new() |> Kino.render()
     assert %KinoTelemetry{metric: ^last_value} = kino
@@ -14,5 +20,16 @@ defmodule KinoTelemetryTest do
     data = connect(kino.vl)
     assert %{spec: %{}, datasets: [[nil, [%{x: x, y: 123}]]]} = data
     assert_in_delta(x, System.system_time(:millisecond), 5)
+  end
+
+  test "only pushes measurements to keep" do
+    last_value = Telemetry.Metrics.last_value("a.b.c", keep: & &1.keep?)
+    kino = last_value |> KinoTelemetry.new() |> Kino.render()
+
+    :telemetry.execute([:a, :b], %{c: 200}, %{keep?: false})
+    :telemetry.execute([:a, :b], %{c: 100}, %{keep?: true})
+
+    data = connect(kino.vl)
+    assert %{spec: %{}, datasets: [[nil, [%{x: _, y: 100}]]]} = data
   end
 end
